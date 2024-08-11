fa0649acb6f5651ceb97230e120b03e6c1ad4c7b
danieiff <sugielff@gmail.com>
2024-08-11 21:17:22 +0900

    Add todo routes, split files

--- src/todo_clj/core.clj
+++ src/todo_clj/core.clj
@@ -1,52 +1,15 @@
 (ns todo-clj.core
   (:require [ring.adapter.jetty :as server]
-            [ring.util.response :as res]
-            [compojure.core :refer [defroutes context GET]]
-            [compojure.route :as route]))
+            [compojure.core :refer [routes]]
+            [todo-clj.handler.todo :refer [main-routes]]
+            [todo-clj.handler.todo :refer [todo-routes]]))
 
 (defonce server (atom nil))
 
-(defn html [res]
-  (res/content-type res "text/html; charset=utf-8"))
-
-(defn home-view [req]
-  "<h1>Home</h1>
-   <a href=\"/todo\">TODO List</a>")
-
-(defn home [req]
-  (-> (home-view req)
-      res/response
-      html))
-
-(def todo-list
-  [{:title "eat"}
-   {:title "sleep"}
-   {:title "slay"}
-   {:title "repeat"}])
-
-(defn todo-index-view [req]
-  `("<h1>TODO List</h1>"
-    "<ul>"
-    ~@(for [{:keys [title]} todo-list]
-        (str "<li>" title "</li>"))
-    "</ul>"))
-
-(defn todo-index [req]
-  (-> (todo-index-view req)
-      res/response
-      html))
-
-(def routes
-  {"/" home
-   "/todo" todo-index})
-
-(defn match-route [uri]
-  (get routes uri))
-
-(defroutes handler
-  (GET "/" req home)
-  (GET "/todo" req todo-index)
-  (route/not-found "<h1> 404 Page Not Found</h1>"))
+(def app
+  (routes
+   todo-routes
+   main-routes))
 
 (defn start-server []
   (when-not @server
